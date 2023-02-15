df023fbd0eb1809a930e0a1d8cfb1e3614ced332
priyanka109905 <72695803+priyanka109905@users.noreply.github.com>
2023-02-15 13:14:55 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,36 @@
+/*
+ARRAYS
+To Store we use arrays
+Arrays is an object in javascript
+*/
+/*
+let selectecolors=["red","blue"];
+let change=selectecolors.toString;
+console.log(selectecolors);
+console.log(selectecolors);
+console.log(selectecolors[0]);
+
+selectecolors[2]='green';
+console.log(selectecolors);
+
+// we can store number also
+selectecolors[3]=1;
+console.log(selectecolors);
+
+//Properties Of Array
+//LENGTH = numbers of items or ele
+console.log(selectecolors.length);
+*/
+let a='Green'
+console.log(a.length);
+let b='';
+for(let i=a.length-1;i>=0;i--){
+
+   //
+    console.log(a[i]);
+    b=b.concat(a[i]);
+}
+console.log(b);
+
+
+
---
+++ operator.js
@@ -0,0 +1,18 @@
+// OPERATOR IN JS
+
+let a=4;
+let b=2;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+console.log(a-b);
+console.log(a++);
+console.log(a);
+console.log(a--);
+// ++a will make the first(+) and will then print a
+//a++ will print the first a
+
+
